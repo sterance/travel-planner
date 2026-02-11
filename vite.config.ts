a3fe2ee552db88883ffdc41d3a9d34076d9c9ee1
sterance <smithc9083@gmail.com>
2026-02-12 00:16:52 +1100

    changed gh actions workflow to produce stats.html as an artifact

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }),
+    visualizer({
+      filename: 'stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
 })
